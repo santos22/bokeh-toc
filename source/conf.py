468d4a2451b53e60301f35f2c4fd305545728abd
santos22 <santosjavier22@gmail.com>
2020-07-23 22:01:29 -0700

    Fix autodoc_default_flags

--- source/conf.py
+++ source/conf.py
@@ -54,9 +54,7 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-autodoc_default_options = {
-    'members': True,
-}
+autodoc_default_flags = ['members']
 
 autosummary_generate = True
 
